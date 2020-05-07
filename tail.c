b13891cf72eb1ce71db4146dbc561694e3cceb14
hipzi <zahramillah582@gmail.com>
2020-05-07 17:13:49 +0700

    update tail

--- tail.c
+++ tail.c
@@ -5,7 +5,7 @@
 char buf[1024];
 char buf2[1024];
 
-void tail(int fd,char *name, int x)
+void tail(int fd,char *file, int x)
 {
 	int i,n,m,l;
 	int tot_lines;
@@ -18,14 +18,15 @@ void tail(int fd,char *name, int x)
 		{
 			if(buf[i]=='\n')
 			{
-			   tot_lines++;  // Loop for total number of lines in the file
-			   if(strcmp(name,"")==0){
-				printf(1,"\n");}
+			   tot_lines++; 
+			   if(strcmp(file,"")==0){
+			    printf(1,"\n");
+               }
 			}
 			else
 			{
-			if(strcmp(name,"")==0){
-				if(buf[i] =='\0')  // Check end of file
+			if(strcmp(file,"")==0){
+				if(buf[i] =='\0')
 				{
 					exit();
 				}
@@ -45,7 +46,7 @@ void tail(int fd,char *name, int x)
 	close(fd);
 	start = tot_lines-x;
 	l=0;
-	int fd2 = open(name,0); //Creating file descriptor 2
+	int fd2 = open(file,0);
 	while((m=read(fd2,buf2,sizeof(buf2)))>0)
 	{
 		for(i=0;i<=m;i++)
@@ -67,25 +68,13 @@ void tail(int fd,char *name, int x)
 		}
 	}
 	close(fd2);
-	if(n<0)
-	{
-		printf(1,"tail: error while reading \n");
-		exit();
-	}
 }
 
-int
-main(int argc,char *argv[])
+int main(int argc,char *argv[])
 {
 	int fd,i;
 
-	if(argc<=1)
-	{
-		tail(0,"",10);
-		exit();
-	}
-
-	else if(argc==2)
+	if(argc==2)
 	{
 		for(i=1;i<argc;i++)
 		{
@@ -107,7 +96,7 @@ main(int argc,char *argv[])
 		strcpy(c,argv[1]);
 		char *num_str=c;
 		num_str=num_str+1;
-		int x= atoi(num_str);
+		int x = atoi(num_str);
 
 		for(i=2;i<argc;i++)
 		{
@@ -120,7 +109,6 @@ main(int argc,char *argv[])
 			close(fd);
 		}
 	}
-
 	else
 	{
 		printf(1,"tail: syntax error");
