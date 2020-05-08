5f6bc0df582c66bcc45f775f2618d3482d0cb6de
hipzi <zahramillah582@gmail.com>
2020-05-08 15:04:31 +0700

    command chown

--- chown.c
+++ chown.c
@@ -1,53 +1,46 @@
+#include "types.h"
+#include "stat.h"
 #include "user.h"
-#include "errno.h"
-#include "pwd.h"
-#include "grp.h"
 
-int main(int argc, char** argv)
+int 
+uchown(char* file_name, uint new_UIDNumb){
+
+	if(new_UIDNumb == -1){
+		printf(1, "%s UID is %d\n",file_name, chown(file_name, -1));
+	}else{
+		if(getuid() == 0){
+			return chown(file_name, new_UIDNumb);
+		}else{
+			printf(1, "Invalid Permissions must be root\n");
+			return -3;
+		}
+	}
+		return 0;
+}
+
+int
+main(int argc, char **argv)
 {
-  if (argc != 3) {
-    printf(2, "Usage: chown username file\n");
-    exit();
-  }
-  char* owner = argv[1];
-  char* group;
-  if ((group = strchr(argv[1], ':'))) {
-    *group = 0;
-    group++;
-  }
-  struct passwd* pass = 0;
-  uid_t uid = -1;
-  if (owner[0]) {
-    pass = getpwnam(owner);
-    if (pass == 0) {
-      printf(2, "Invalid user: %s\n", owner);
-      exit();
-    }
-    uid = pass->pw_uid;
-  }
-  struct group* grp = 0;
-  gid_t gid = -1;
-  if (group) {
-    grp = getgrnam(group);
-    if (grp == 0) {
-      printf(2, "Invalid group: %s\n", grp);
-      exit();
-    }
-    gid = grp->gr_gid;
-  }
-  if (chown(argv[2], uid, gid) < 0) {
-    switch (errno) {
-      case EPERM:
-        printf(2, "Permission denied\n");
-        break;
-      case ENOENT:
-        printf(2, "No such file or directory\n");
-        break;
-      default:
-        printf(2, "Unexpected errno: %d\n", errno);
-        break;
-    }
-  }
+	if(argc != 3){
+		printf(1, "Please give a file, and a User ID\n");
+		exit();
+	}
+	if((int)*argv[1] == 63){
+		uchown(argv[2], -1);
+	}else{
+		int x = uchown(argv[2], atoi(argv[1]));
 
-  exit();
+		if(-1 == x){
+			printf(1, "Error: Not a valid UID\n");
+		}
+		if(-2 == x){
+			printf(1, "Error: File Name Invalid\n");
+		}
+		if (0 == x)
+		{
+			printf(1,"%s%s%s%s\n", "You changed owner ", argv[2], " to ", argv[1]);
+		}
+	}
+	
+ 	 exit();
 }
